2309ba5100854cb21ba7d407ea1cfd8ab1e3eb64
tongzou <tong@cal.berkeley.edu>
2017-06-28 22:47:24 -0700

    minor update

--- src/simplenn.py
+++ src/simplenn.py
@@ -21,22 +21,22 @@ for j in xrange(600000):
 print 'l2=', l2
 '''
 
-def nonlin(x,deriv=False):
-    if(deriv==True):
-        return x*(1-x)
-
+def sigmoid(x):
     return 1/(1+np.exp(-x))
 
-X = np.array([[0,0,1],
+def sigmoid_prime(x):
+    return sigmoid(x) * (1 - sigmoid(x))
+
+'''X = np.array([[0,0,1],
               [0,1,1],
               [1,0,1],
               [1,1,1]])
-y = np.array([[0],[1],[1],[0]])
-#X = np.array([[1,0,1]])
-#y = np.array([[1]])
+y = np.array([[0],[1],[1],[0]])'''
+X = np.array([[1,0,1]])
+y = np.array([[1]])
 
 np.random.seed(1)
-eta = 1.0
+eta = 3.0
 
 # randomly initialize our weights with mean 0
 #w1 = 2*np.random.random((3,4)) - 1
@@ -50,9 +50,9 @@ for j in xrange(600000):
 
     # Feed forward through layers 0, 1, and 2
     z1 = X.dot(w1)
-    a1 = nonlin(z1)
+    a1 = sigmoid(z1)
     z2 = a1.dot(w2)
-    a2 = nonlin(z2)
+    a2 = sigmoid(z2)
     #print 'a1=', a1
     #print 'a2=', a2
 
@@ -63,21 +63,16 @@ for j in xrange(600000):
     if (j % 10000) == 0:
         print "Error:" + str(np.mean(np.abs(da2)))
 
-    # in what direction is the target value?
-    # were we really sure? if so, don't change too much.
-    dz2 = da2*nonlin(a2, deriv=True)
+    dz2 = da2 * sigmoid_prime(z2)
     #print 'dz2=', dz2
 
-    # how much did each a1 value contribute to the a2 error (according to the weights)?
     da1 = dz2.dot(w2.T)
     #print 'da1=', da1
 
-    # in what direction is the target a1?
-    # were we really sure? if so, don't change too much.
-    dz1 = da1 * nonlin(a1, deriv=True)
+    dz1 = da1 * sigmoid_prime(z1)
     #print 'dz1=', dz1
 
-    #dx = dz1.dot(w1.T)
+    dx = dz1.dot(w1.T)
     #print 'dx=', dx
 
     dw2 = a1.T.dot(dz2)
@@ -86,5 +81,5 @@ for j in xrange(600000):
     dw1 = X.T.dot(dz1)
     #print 'dw1=', dw1
 
-    w2 -= 4*dw2/4
-    w1 -= 4*dw1/4
+    w2 -= eta*dw2/4
+    w1 -= eta*dw1/4
