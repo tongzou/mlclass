d54c0e9af64cb8ed255062d28e72b4f2896cedda
tongzou <tong@cal.berkeley.edu>
2017-07-02 12:02:14 -0700

    updated

--- src/simplenn.py
+++ src/simplenn.py
@@ -27,13 +27,13 @@ def sigmoid(x):
 def sigmoid_prime(x):
     return sigmoid(x) * (1 - sigmoid(x))
 
-'''X = np.array([[0,0,1],
+X = np.array([[0,0,1],
               [0,1,1],
               [1,0,1],
               [1,1,1]])
-y = np.array([[0],[1],[1],[0]])'''
-X = np.array([[1,0,1]])
-y = np.array([[1]])
+y = np.array([[0],[1],[1],[0]])
+#X = np.array([[1,0,1]])
+#y = np.array([[1]])
 
 np.random.seed(1)
 eta = 3.0
@@ -46,40 +46,40 @@ w1 = np.array([[0.1, 0.2, 0.3, 0.4],
                [0.3, 0.2, -0.1, -0.2]])
 w2 = np.array([[0.3, -0.2, 0.5, -0.4]]).T
 
-for j in xrange(600000):
+for j in xrange(1):
 
     # Feed forward through layers 0, 1, and 2
     z1 = X.dot(w1)
     a1 = sigmoid(z1)
     z2 = a1.dot(w2)
     a2 = sigmoid(z2)
-    #print 'a1=', a1
-    #print 'a2=', a2
+    print 'a1=', a1
+    print 'a2=', a2
 
     # how much did we miss the target value?
     da2 = a2-y
-    #print 'da2=', da2
+    print 'da2=', da2
 
     if (j % 10000) == 0:
         print "Error:" + str(np.mean(np.abs(da2)))
 
     dz2 = da2 * sigmoid_prime(z2)
-    #print 'dz2=', dz2
+    print 'dz2=', dz2
 
     da1 = dz2.dot(w2.T)
-    #print 'da1=', da1
+    print 'da1=', da1
 
     dz1 = da1 * sigmoid_prime(z1)
-    #print 'dz1=', dz1
+    print 'dz1=', dz1
 
     #dx = dz1.dot(w1.T)
     #print 'dx=', dx
 
     dw2 = a1.T.dot(dz2)
-    #print 'dw2=', dw2
+    print 'dw2=', dw2
 
     dw1 = X.T.dot(dz1)
-    #print 'dw1=', dw1
+    print 'dw1=', dw1
 
     w2 -= eta*dw2/4
     w1 -= eta*dw1/4
